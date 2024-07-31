1120f3d554cf4b1673ffe31b418db4f2773c294a
FlashScript <itzflashscript@gmail.com>
2024-07-31 12:05:33 +0330

    S.O.L.I.D principles

--- src/components/RecipePage.tsx
+++ src/components/RecipePage.tsx
@@ -1,5 +1,6 @@
 import { omeletteImage } from "../assets"
-import { ingredients, ingredientTitle, instructions, instructionTitle, nutritionInfo, nutritions, nutritionTitle, omelettedescription, omeletteTitle, preparInfo, preparTime } from "../constants"
+import { omelettedescription, omeletteTitle } from "../constants"
+import { Ingredients, Instructions, Nutrition, Preparation } from "./"
 
 const RecipePage = () => {
 
@@ -22,95 +23,17 @@ const RecipePage = () => {
                     {omelettedescription}
                 </p>
 
-                <div className={`px-6 py-6 mb-6 |  | bg-neutral-rose-white rounded-xl | `}>
-                    <h5 className={`mb-2 | text-lg text-primary-dark-raspberry font-outfit font-semibold |  | `}>
-                        {preparTime}
-                    </h5>
+                <Preparation />
 
-                    <ul className={`px-6 space-y-[6px] |  |  | `}>
-                        {preparInfo.map((item) => (
-                            <li 
-                                key={item.id}
-                                className={` | text-base font-outfit text-neutral-wenge-brown | list-disc | preparation_li`}
-                            >   
-                                <div className={` |  |  | bullet_mover_preparation`}>
-                                    <span className={` | font-semibold |  | `}>{item.bold}</span>
-                                    {item.normal}
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-
-                <div>
-                    <h3 className={`mb-3 | text-2xl md:text-[1.7rem] text-primary-nutmeg font-young-serif font-normal |  | `}>
-                        {ingredientTitle}
-                    </h3>
-
-                    <ul className={`px-6 space-y-[6px] |  |  | `}>
-                        {ingredients.map((item) => (
-                            <li
-                                key={item.id}
-                                className={` | text-base font-outfit font-normal text-neutral-wenge-brown | list-disc | ingredients_li`}
-                            >
-                                <span className={` |  |  | bullet_mover_ingredients`}>
-                                    {item.text}
-                                </span>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+                <Ingredients />
 
                 <hr className={`my-6 |  |  | `} />
 
-                <div>
-                    <h3 className={`mb-3 | text-2xl md:text-[1.7rem] text-primary-nutmeg font-young-serif font-normal |  | `}>
-                        {instructionTitle}
-                    </h3>
-
-                    <ul className={`pl-6 pr-5 md:pr-12 space-y-[6px] |  |  | `}>
-                        {instructions.map((item) => (
-                            <li
-                                key={item.id}
-                                className={` | text-base font-outfit text-neutral-wenge-brown | list-decimal | instructions_li`}
-                            >
-                                <div className={` |  |  | bullet_mover_instructions`}>
-                                    <span className={` | font-semibold |  | `}>{item.bold}</span>
-                                    {item.normal}
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+                <Instructions />
 
                 <hr className={`my-6 |  |  | `} />
                 
-                <div>
-                    <h3 className={`mb-3 | text-2xl md:text-[1.7rem] text-primary-nutmeg font-young-serif font-normal |  | `}>
-                        {nutritionTitle}
-                    </h3>
-
-                    <p className={`mb-6 pr-4 | text-base text-neutral-wenge-brown font-outfit font-normal |  | `}>
-                        {nutritionInfo}
-                    </p>
-
-                    <div className={`mb-2 md:mb-0 |  |  | `}>
-                        {nutritions.map((item) => (
-                            <div 
-                                key={item.id}
-                            >   
-                                <div className={`px-6 |  | flex  | `}>
-                                    <span className={` | text-base text-neutral-wenge-brown font-outfit font-normal | basis-1/2 | `}>{item.serving}</span>
-                                    <span className={` | text-base text-primary-nutmeg font-outfit font-semibold | basis-1/2 | `}>{item.value}</span>
-                                </div>
-
-                                {item.hr && (
-                                    <hr className={`my-3 mr-4 | bg-black  |  | `} />
-                                )}
-                            </div>
-                        ))}
-                    </div>
-                </div>
+                <Nutrition />
             </div>
         </div>
     )
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,15 @@
 import FlashScript from "./FlashScript";
 import RecipePage from "./RecipePage";
+import Preparation from "./Preparation";
+import Ingredients from "./Ingredients";
+import Instructions from "./Instructions";
+import Nutrition from "./Nutrition";
 
 export {
     FlashScript,
-    RecipePage
+    RecipePage,
+    Preparation,
+    Ingredients,
+    Instructions,
+    Nutrition
 }
